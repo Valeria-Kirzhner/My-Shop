76a095f5c37605177acb4ebb617c04d2f78b6267
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-12 15:21:03 +0300

    added links

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,33 +12,24 @@
     <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="{{ url('') }}">MY SHOP </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="{{url('/about') }}">About</a>
+        </li>
+
+    </ul>
+    <ul class="navbar-nav navbar-right">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{url('user/signin') }}">Sign In</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+          <a class="nav-link active" aria-current="page" href="{{url('user/signup') }}">Sign Up</a>
+        </li> 
+    </ul>
   </div>
 </nav>
     </header>
