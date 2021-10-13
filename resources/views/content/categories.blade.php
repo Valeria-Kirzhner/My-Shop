cc123195396c6bcf02e7cf9ca451c89a955891d8
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-13 18:46:18 +0300

    added dynamic link on each categorie

--- resources/views/content/categories.blade.php
+++ resources/views/content/categories.blade.php
@@ -8,10 +8,13 @@
 <div class="row">
     @foreach ($categories as $category)
     <div class="col-md-4 col-sm-6 pt-10">
+        <a href="{{ url('shop/' .  $category['url']) }}">
         <h3>
             {{$category['title']}}
         </h3>
-        <p><img src="{{ asset('images/' . $category['image'] )}}" width="200" height="225" ></p>
+        
+        <p><img src="{{ asset('images/' . $category['image'] )}}" width="200" height="225"></p>
+        </a>
         <p>{{$category['article']}}</p>
 
     </div>
