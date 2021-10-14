80c1655937ae24a6b5e906568b9083dc91abfbc1
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-14 14:21:18 +0300

    added prifix route to Shop

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\PagesController;
 use App\Http\Controllers\ShopController;
 
+# Pages
 Route::get('/', [PagesController::class, 'home']);
 Route::get('/about', [PagesController::class, 'about']);
-Route::get('/shop', [ShopController::class, 'categories']);
-// Itzhak , how does it knows what is cat_url ???  :-O
-Route::get('/shop/{cat_url}', [ShopController::class, 'products']);
+
+# Shop
+Route::prefix('shop')->group(function() {
+    Route::get('/', [ShopController::class, 'categories']);
+    Route::get('{cat_url}', [ShopController::class, 'products']);
+});
+
 
 
 
