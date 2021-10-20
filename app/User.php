aa9822d14358da73016bb86e04e6e3de824182cd
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-20 16:06:15 +0300

    added functionality to user model to validate and get user that trying to connect.

--- app/User.php
+++ app/User.php
@@ -3,10 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB, Session, Hash;
 
 class User extends Model
 {
-    static public function validate(){
-        return false;
+    static public function validate($request){
+        $valid = false;
+        $email = $request['email'];
+        $password = $request['password'];
+        $sql = "SELECT * FROM users u "
+                . "JOIN user_roles r ON u.id=r.uid "
+                . "WHERE u.email = ?";
+        $user = DB::select($sql, [$email]);
+
+        if($user){
+            $user = $user[0];
+            if( Hash::check($password, $user->password )){
+                $valid = true;
+                if($user->role == 6) Session::put('is_admin', true);
+                Session::put('user_id', $user->id);
+                Session::put('user_name', $user->name);
+                Session::flash('sm', $user->name . ' welcome back!' );
+
+            }
+        }
+
+        return $valid;
     }
 }
