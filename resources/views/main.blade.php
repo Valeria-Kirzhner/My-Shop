ca6706e60eead811883f922e60a58590534b5051
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-20 14:35:43 +0300

    added laravel function to render error messages for signin

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -56,6 +56,20 @@
           </div>
         </div>
         @endIf
+            @if ($errors->any())
+              <div class="row">
+                <div class="col-md-12">
+                  <br>
+                  <div class="alert alert-danger">
+                    <ul>
+                      @foreach ($errors->all() as $error) 
+                        <li>{{$error}}</li>
+                      @endforeach
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              @endif
         @yield('home') 
         @yield('about') 
       </div>
