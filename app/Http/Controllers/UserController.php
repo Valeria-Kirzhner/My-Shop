01d0983718f6a79a1b9907b00cdc4ccbe3650b40
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-20 14:26:38 +0300

    created User model and controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
+use App\Http\Requests\SigninRequest;
 
 class UserController extends MainController
 {
-    //
+    public function getSignin(){
+        self::$data['title'] .= 'sign in page';
+        return view('forms.signin',  self::$data);
+    }
+    public function postSignin(SigninRequest $request){// dependency injection -> the request must first get over validation
+        echo __METHOD__;
+    }
+
 }
