d38ed5f6989e01a0f59b29e13d85463852d20868
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-21 21:30:27 +0300

    the order of the routes changed

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use  App\Http\Controllers\PagesController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 
-
-# Pages
-Route::get('/', [PagesController::class, 'home']);
-Route::get('{url}', [PagesController::class, 'content']);
-
 # Shop
 Route::prefix('shop')->group(function() {
     Route::get('/', [ShopController::class, 'categories']);
@@ -19,23 +14,22 @@ Route::prefix('shop')->group(function() {
     Route::get('/update-cart', [ShopController::class, 'updateCart']);
     Route::get('/remove-item/{id}', [ShopController::class, 'removeItem']);
     Route::get('/checkout', [ShopController::class, 'checkout']);
-    Route::get('{cat_url}', [ShopController::class, 'products']);
     Route::get('{cat_url}/{prd_url}', [ShopController::class, 'item']);
+    Route::get('{cat_url}', [ShopController::class, 'products']);
 });
 
 # User
 Route::prefix('user')->group(function() {
-
     Route::get('signin', [UserController::class, 'getSignin']); 
     Route::post('signin', [UserController::class, 'postSignin']);
     Route::get('signup', [UserController::class, 'getSignup']); 
     Route::post('signup', [UserController::class, 'postSignup']);  
     Route::get('logout', [UserController::class, 'logout']); 
-
-
 });
 
-
+# Pages
+Route::get('/', [PagesController::class, 'home']);
+Route::get('{url}', [PagesController::class, 'content']);
 
 
 
