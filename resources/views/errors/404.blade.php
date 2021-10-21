4343fbe0fe07f7e19a83a06c2008eb91df8e02c9
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-21 23:06:15 +0300

    apage 404 created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<?php
+
+$title = 'MY SHOP | page 404';
+$menu = App\Menu::all()->toArray();
+$errors = [];
+?>
+
+@extends('main')
+@section('home')
+<div class="row">
+    <div class="col-md-12">
+        <br><br>
+        <div class="alert alert-warning" role="alert">
+            <h3>OOPS...! The page you looking is not found!</h3>
+            <p>Error 404</p>
+        </div>
+    </div>
+</div>
+    
+@endsection
