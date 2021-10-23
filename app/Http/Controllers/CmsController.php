7dd5e020585547539e8b010b4c83cc7782768eff
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-23 19:26:55 +0300

    dashboard links are changed and first dashboard partial created

--- app/Http/Controllers/CmsController.php
+++ app/Http/Controllers/CmsController.php
@@ -8,6 +8,6 @@ use DB;
 class CmsController extends MainController
 {
     public function dashboard () {
-        return view('cms.cms_main', self::$data);
+        return view('cms.dashboard', self::$data);
     }
 }
