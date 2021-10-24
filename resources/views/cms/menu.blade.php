fb29adde465e8618d76717122abd15df654aa122
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-10-24 15:17:38 +0300

    style correction

--- resources/views/cms/menu.blade.php
+++ resources/views/cms/menu.blade.php
@@ -3,10 +3,11 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Edit Site Menu</h1>
 </div>
+<br><br>
   <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-8">
       <p>
-        <a class="btn btn-primary" href="{{ url('cms/menu/create')}}">+ Add new menu</a>
+        <a class="btn btn-primary" href="{{ url('cms/menu/create')}}">+ New menu</a>
       </p>
       <table class="table table-bordered">
         <thead>
@@ -21,15 +22,13 @@
               <td>{{ $item['link']}}</td>
               <td>{{ $item['updated_at']}}</td>
               <td>
-                <a href="#">Edit</a>
+                <a href="#">Edit</a> | 
                 <a href="#">Delete</a>
               </td>
             </tr>
         @endforeach
       </tbody>
-
       </table>
-
     </div>
   </div>
 
