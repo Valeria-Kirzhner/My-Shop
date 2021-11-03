2df4c21297a56cca63b0e67f015caaba55b5646d
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-11-03 17:28:08 +0200

    added to Menu model sql to save new menu

--- app/Menu.php
+++ app/Menu.php
@@ -9,6 +9,11 @@ class Menu extends Model
 {
     static public function save_new($request) {
         $menu = new self();
-        
+        $menu->link = $request['link'];
+        $menu->mtitle = $request['mtitle'];
+        $menu->url = $request['url'];
+        $menu->save();
+        Session::flash('sm', 'Menu has been saved');
+
     }
 }
