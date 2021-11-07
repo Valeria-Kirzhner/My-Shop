80ef2ab7f058521d43452355aae8133d4bb14b6d
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-11-07 21:12:22 +0200

    added content creation functionality

--- resources/views/cms/content.blade.php
+++ resources/views/cms/content.blade.php
@@ -1,13 +1,13 @@
 @extends('cms/cms_main')
 @section('cms_content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Edit Site Menu</h1>
+    <h1 class="h2">Edit Site Content</h1>
 </div>
 <br><br>
   <div class="row">
     <div class="col-lg-8">
       <p>
-        <a class="btn btn-primary" href="{{ url('cms/menu/create')}}">+ New menu</a>
+        <a class="btn btn-primary" href="{{ url('cms/content/create')}}">+ New content</a>
       </p>
       <table class="table table-bordered">
         <thead>
@@ -18,14 +18,14 @@
         </tr>
       </thead>
       <tbody>
-        @foreach ($menu as $item)
+        @foreach ($content as $item)
             <tr>
-              <td>{{ $item['link']}}</td>
+              <td>{{ $item['title']}}</td>
               <td>{{ $item['updated_at']}}</td>
               <td>{{ $item['operation']}}</td>
               <td>
-                <a href="{{ url('cms/menu/' . $item['id'] . '/edit')}}">Edit</a> | 
-                <a href="{{ url('cms/menu/' . $item['id']) }}">Delete</a>
+                <a href="{{ url('cms/content/' . $item['id'] . '/edit')}}">Edit</a> | 
+                <a href="{{ url('cms/content/' . $item['id']) }}">Delete</a>
               </td>
             </tr>
         @endforeach
