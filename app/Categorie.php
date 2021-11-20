0df2eb1d8d0b200933c19272e469ed119f5a4d64
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-11-20 15:12:14 +0200

    added option to load photo in sequre way to Categories table

--- app/Categorie.php
+++ app/Categorie.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Session;
+use Image, Session;
 
 class Categorie extends Model
 {
@@ -21,6 +21,14 @@ class Categorie extends Model
             $file = $request->file('image');
             $image_name = date('Y.m.d.H.i.s') . '-' . $file->getClientOriginalName();
             $request->file('image')->move(public_path() . '/images', $image_name);
+            //build the img to be sure it isn't have no viruses.
+            $img = Image::make(public_path() . '/images/' . $image_name);
+            $img->resize(300, null, function($constraint){
+                $constraint->aspectRatio();// to keep width relative to the height. 
+            });
+            $img->save(public_path() . '/images/' . 'w-'. $image_name);// the w is to mark that this is the new one.
+            unlink(public_path() . '/images/' . $image_name);
+            $image_name = 'w-'. $image_name;
         }
         $category = new self();
         $category->title = $request['title'];
